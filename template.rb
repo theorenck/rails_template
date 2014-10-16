e0f94d3d97f4c3d6b47e742ee2f28a15a54b3bc0
Theo Renck <carlosrenck@gmail.com>
2014-10-16 07:09:22 -0300

    add database configuration and git initialization

--- template.rb
+++ template.rb
@@ -1,17 +1,3 @@
-# Copy files directly from repo
-def copy_from_repo(filename, options = {})
-  repo = 'https://raw.github.com/theorenck/rails_template/master/'
-  repo = options[:repo] unless options[:repo].nil?
-  source_filename = filename
-  destination_filename = filename
-  begin
-    remove_file destination_filename
-    get repo + source_filename, destination_filename
-  rescue OpenURI::HTTPError
-    say "Unable to obtain #{source_filename} from the repo #{repo}"
-  end
-end
-
 # Remove sqlite3 as default database adapter
 gsub_file 'Gemfile', /gem 'sqlite3'\n/, ''
 
@@ -33,13 +19,50 @@ gem 'font-awesome-sass', '~> 4.1.0'
 # 12factor (Heroku deployment) configuration compliance 
 gem 'rails_12factor', group: :production
 
-# Add a module for CPF/CNPJ calculation
-copy_from_repo 'lib/pessoa_utils.rb'
+# Copy files directly from repo
+def copy_from_repo(filename, options = {})
+  repo = 'https://raw.github.com/theorenck/rails_template/master/'
+  repo = options[:repo] unless options[:repo].nil?
+  source_filename = filename
+  destination_filename = filename
+  begin
+    remove_file destination_filename
+    get repo + source_filename, destination_filename
+  rescue OpenURI::HTTPError
+    say "Unable to obtain #{source_filename} from the repo #{repo}"
+  end
+end
 
-# Add a presenter base class
-copy_from_repo 'app/presenters/base_presenter.rb'
+# Run after bundle execution
+after_bundler do
 
-# Add CPF,CNPJ and Email validators
-copy_from_repo 'app/validators/cpf_validator.rb'
-copy_from_repo 'app/validators/cnpj_validator.rb'
-copy_from_repo 'app/validators/email_validator.rb'
+  # Add a module for CPF/CNPJ calculation
+  copy_from_repo 'lib/pessoa_utils.rb'
+
+  # Add a presenter base class
+  copy_from_repo 'app/presenters/base_presenter.rb'
+
+  # Add CPF,CNPJ and Email validators
+  copy_from_repo 'app/validators/cpf_validator.rb'
+  copy_from_repo 'app/validators/cnpj_validator.rb'
+  copy_from_repo 'app/validators/email_validator.rb'
+
+  # Configure postgresql database
+  copy_from_repo 'config/database.yml'
+  begin
+    say "Creating a user named '#{app_name}' for PostgreSQL"
+    gsub_file "config/database.yml", /database: myapp_development/, "database: #{app_name}_development"
+    gsub_file "config/database.yml", /database: myapp_test/,        "database: #{app_name}_test"
+    gsub_file "config/database.yml", /database: myapp_production/,  "database: #{app_name}_production"
+  rescue StandardError
+    raise "unable to create database configuration for PostgreSQL"
+  end
+end
+
+# Run at the end of the template
+after_everything do
+  # Initialize git repo, add and commit
+  git :init
+  git add: %Q{ --all }
+  git commit: %Q{ -m 'Initial commit' }
+end
