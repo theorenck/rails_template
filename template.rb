4a465b8c3917bc142f9c5ded8199471109c9ac41
Theo Renck <carlosrenck@gmail.com>
2014-10-27 15:39:32 -0200

    add twitter bootstrap support for layout and view generation

--- template.rb
+++ template.rb
@@ -10,21 +10,21 @@ gem 'pg'
 append_to_file  'Gemfile', "\n\n\# Replace jbuilder"
 gem 'active_model_serializers', '~> 0.8.1'
 
-# Use new relic extension for performance monitoring
-append_to_file  'Gemfile', "\n\n\# Use new relic extension for performance monitoring"
-gem 'newrelic_rpm'
+# Use LESS as dynamic stylesheet language for the Rails asset pipeline
+append_to_file  'Gemfile', "\n\n\# Use LESS as dynamic stylesheet language for the Rails asset pipeline"
+gem 'less-rails', '~> 2.5.0'
 
-# Use a Sass-powered version of Bootstrap
-append_to_file  'Gemfile', "\n\n\# Use a Sass-powered version of Bootstrap"
-gem 'bootstrap-sass', '~> 3.2.0'
-
-# Use a Sass-powered version of font-awesome icon library
-append_to_file  'Gemfile', "\n\n\# Use a Sass-powered version of font-awesome icon library"
-gem 'font-awesome-sass', '~> 4.1.0'
+# Use Twitter Bootstrap view and layout generator
+append_to_file  'Gemfile', "\n\n\# Use Twitter Bootstrap view and layout generator"
+gem 'twitter-bootstrap-rails', '~> 3.2.0'
 
 # 12factor (Heroku deployment) configuration compliance
 append_to_file  'Gemfile', "\n\n\# 12factor (Heroku deployment) configuration compliance" 
 gem 'rails_12factor', group: :production
+
+# Use new relic extension for performance monitoring
+append_to_file  'Gemfile', "\n\n\# Use new relic extension for performance monitoring"
+gem 'newrelic_rpm'
 
 # Copy files directly from repo
 def copy_from_repo(filename, options = {})
@@ -55,14 +55,18 @@ copy_from_repo 'app/validators/email_validator.rb'
 copy_from_repo 'config/database.yml'
 begin
   say "Creating a database configuration '#{app_name}' for PostgreSQL"
-  gsub_file "config/database.yml", /database: myapp_development/, "database: #{app_name}_development"
-  gsub_file "config/database.yml", /database: myapp_test/,        "database: #{app_name}_test"
-  gsub_file "config/database.yml", /database: myapp_production/,  "database: #{app_name}_production"
+  gsub_file "config/database.yml", /database: myapp_development/, "database: #{app_name.underscore}_development"
+  gsub_file "config/database.yml", /database: myapp_test/,        "database: #{app_name.underscore}_test"
+  gsub_file "config/database.yml", /database: myapp_production/,  "database: #{app_name.underscore}_production"
 rescue StandardError
   raise "unable to create database configuration for PostgreSQL"
 end
 
 run 'bundle install --without production'
+
+# Install and create bootstrap layouts
+run 'rails g bootstrap:install'
+run 'rails g bootstrap:layout application'
 
 # Initialize git repo, add and commit
 git :init
